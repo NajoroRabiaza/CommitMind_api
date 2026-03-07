242a2a08d25e1e01f326566a38d0ccb237e8ff3f
NajoroRabiaza <hei.najoro.2@gmail.com>
2026-03-07 17:32:47 +0300

    feat: add health check route

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const healthRouter = require('./routes/health');
 const app = express();
 
 // Middleware pour lire les JSON dans les requetes
 app.use(express.json());
+app.use(healthRouter)
 module.exports = app;
+

250630f008d0be3493915e1de2eeb6a283183b51
NajoroRabiaza <hei.najoro.2@gmail.com>
2026-03-25 00:32:21 +0300

    chore: prepare app for production

--- src/middleware/passport.js
+++ src/middleware/passport.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: 'http://localhost:3000/auth/github/callback'
+      callbackURL: process.env.CALLBACK_URL || 'http://localhost:3000/auth/github/callback'
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
